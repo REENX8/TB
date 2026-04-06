fd422856f7c470658a749dbdb242a3747a327e41
REENX8 <asdawesdzd22@gmail.com>
2026-04-07 06:29:24 +0700

    Database fixed

--- app.py
+++ app.py
@@ -34,7 +34,10 @@ import qrcode
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///tb.db")
+_db_url = os.environ.get("DATABASE_URL", "sqlite:///tb.db")
+if _db_url.startswith("postgres://"):
+    _db_url = _db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = _db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = os.environ.get("SECRET_KEY", "change_this_secret")
 db = SQLAlchemy(app)
